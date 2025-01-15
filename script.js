e7bef082b384fb39e25a3c789f1d00d00096dc6b
VANSHI200 <patel.vanshi@northeatern.edu>
2025-01-15 00:27:03 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
